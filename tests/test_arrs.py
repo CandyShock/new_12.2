71b33b5006f815a1841cd11e18e5dc574fe5584c
CandyShock <mrhell699@gmail.com>
2023-03-21 20:56:47 +0300

    s

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,6 +8,11 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
         self.assertEqual(arrs.get([], 0, "test"), "test")
 
+        #with self.
+
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+if __name__ == '__main__':
+    unittest.main()
